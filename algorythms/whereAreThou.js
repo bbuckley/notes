d325a2d7d351bb74a61982f103f90e26963b608c
Brian K. Buckley <briankbuckley@gmail.com>
2016-06-16 18:13:57 -0400

    Add a .js file

---
+++ algorythms/whereAreThou.js
@@ -0,0 +1,24 @@
+function whereAreYou(collection, source) {
+  // What's in a name?
+  var arr = [];
+  // Only change code below this line
+  var e;
+  for(var i; i < collection.length; i++){
+   
+  e = collection[i];
+  for (var key in e) {
+  if (e.hasOwnProperty(key) && sour) {
+
+  }
+
+
+
+}
+
+  
+  
+  // Only change code above this line
+  return arr;
+}
+
+whereAreYou([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
