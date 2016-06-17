0f47d4b78af2743795fd8abe1393da0b65fa830e
Brian K. Buckley <briankbuckley@gmail.com>
2016-06-17 16:07:29 -0400

    Edit mutations

--- algorythms/mutations.js
+++ algorythms/mutations.js
@@ -1,8 +1,14 @@
-require 'set'
+function mutation(arr) {
+  
+var x = arr[0].toLowerCase(); 
+for (var i = 0; i < arr[1].length; i++) {
+  
+  if(x.indexOf(arr[1][i].toLowerCase()) == -1){
+     return false;
+  }
+}
+  
+return true;
+}
 
-var s1  = new Set();
-
-
-
- 
-console.log("s1");
+console.log(mutation(["hello", "hey"]));
