c1bd732bf843018f4b0622a062c8ad948ee2b72b
Brian K. Buckley <briankbuckley@gmail.com>
2016-06-19 22:32:16 -0400

    Add replace.js

---
+++ algorythms/replace.js
@@ -0,0 +1,28 @@
+
+String.prototype.capitalizeFirstLetter = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+String.prototype.lowercaseFirstLetter = function() {
+    return this.charAt(0).toLowerCase() + this.slice(1);
+}
+
+function myReplace(str, before, after) {
+  if (before[0] == before[0].toUpperCase()){
+    after = after.capitalizeFirstLetter();
+  	console.log(after);    
+  }
+  if (before[0] == before[0].toLowerCase()){
+    after = after.lowercaseFirstLetter();
+  	console.log(after);    
+  }
+
+  console.log(after);
+
+  str = str.replace(before, after);
+  
+  
+  return str;
+}
+
+
+console.log(myReplace("His name is Tom", "Tom", "john"));
