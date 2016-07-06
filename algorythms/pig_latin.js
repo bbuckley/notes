2147d7f88cdaf1ce50e8865a33f62bf7bf044540
Brian K. Buckley <briankbuckley@gmail.com>
2016-07-06 13:54:36 -0400

    Tweak pig_latin.js

--- algorythms/pig_latin.js
+++ algorythms/pig_latin.js
@@ -5,7 +5,6 @@ String.prototype.beginsWithVowel = function(){
 	return "aeiou".split("").indexOf(this[0].toLowerCase()) !== -1
 }
 
-
 function translatePigLatin(str) {
    var nonv = str.split(/[aeiou]/)[0].length
    if (nonv == 0){
@@ -20,6 +19,6 @@ console.log(translatePigLatin("paragraphs")) ; //"aragraphspay".
 console.log(translatePigLatin("glove") ); // "oveglay".
 console.log(translatePigLatin("algorithm")); // "algorithmway".
 console.log(translatePigLatin("eight")); //"eightway".translatePigLatin("stragiht"));
-console.log(translatePigLatin("")); 
-console.log(translatePigLatin("a")); 
-console.log(translatePigLatin("s")); 
+console.log(translatePigLatin(""));
+console.log(translatePigLatin("a"));
+console.log(translatePigLatin("s"));
