f476279ace42f6bcdfab5a6cb2c21abd8e213f59
Brian Buckley <briankbuckley@gmail.com>
2016-07-08 17:01:49 -0400

    Misc.

--- algorythms/mutations.js
+++ algorythms/mutations.js
@@ -1,13 +1,13 @@
 function mutation(arr) {
-  
-var x = arr[0].toLowerCase(); 
+
+var x = arr[0].toLowerCase();
 for (var i = 0; i < arr[1].length; i++) {
-  
+
   if(x.indexOf(arr[1][i].toLowerCase()) == -1){
      return false;
   }
 }
-  
+
 return true;
 }
 
