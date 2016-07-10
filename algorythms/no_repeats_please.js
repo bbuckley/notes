8d2a8420c20163ca24a473b7bbb8e9a0fa189e4e
brian buckley <briankbuckley@gmail.com>
2016-07-10 11:38:56 -0400

    Tweak no_repeats_please.js

--- algorythms/no_repeats_please.js
+++ algorythms/no_repeats_please.js
@@ -1,12 +1,29 @@
+
+
 function permAlone(str) {
-  return str;
+  var f = function permute(res, item, key, arr) { return res.concat(
+    arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1))
+      .reduce(permute, [])
+      .map(function(perm) { return [item].concat(perm); }) || item);
+  };
+
+  var g = function(a){
+    for(var i = 1; i < a.length; i++){
+      if(a[i-1]==a[i]){ return false; };
+    };
+    return true;
+  };
+
+  return str.split("").reduce(f,[]).filter(g).length;
 }
 
-permAlone('aab');
+console.log(permAlone('aab'));
 
 Array.prototype.permute = function(){
   return this.reduce(function permute(res, item, key, arr) {
-      return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);
+      return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1))
+      .reduce(permute, [])
+      .map(function(perm) { return [item].concat(perm); }) || item);
   }, []);
 };
 
@@ -14,19 +31,18 @@ Array.prototype.permute = function(){
 var inputArray = [1, 2, 2];
 inputArray =  "abbc".split("");
 var result = inputArray.reduce(function permute(res, item, key, arr) {
-    return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);
+    return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1))
+    .reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);
 }, []);
 
-console.log(result);
-
-console.log("aab".split("").permute());
+//console.log(result);
+//console.log("aab".split("").permute());
 
 
 var a = "1122".split("").permute().filter(function(a){
-  var len = a.length;
   for(var i = 1; i < a.length; i++){
     if(a[i-1]==a[i]){ return false; };
   };
   return true;
 });
-console.log(a)
+//console.log(a)
