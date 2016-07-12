acfc7605c90bfca16108f65d5bfea599b8b76547
Brian Buckley <briankbuckley@gmail.com>
2016-07-12 19:38:17 -0400

    More tester.js

--- algorythms/tester.js
+++ algorythms/tester.js
@@ -3,10 +3,11 @@ atom
 sublime
 vim
 */
-
+//Copyright (c) 2016 Copyright Holder All Rights Reserved.
 var a = [1,2,3];
 
 for (var i = 0; i < a.length; i++) {
    a[i];
   // console.log(a,i,a[i]);
+  console.log();
 };
